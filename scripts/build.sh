d5293825694dace508c502031381a054e802a166
code2358 <code2358@outlook.com>
2018-12-02 11:10:20 +0100

    Build: Adds GPG key ID to version control

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,12 +7,12 @@ set -u
 BUILD_VERSION="$(git rev-list --count master)-$(git rev-parse --short=7 HEAD)"
 OPTIONS=""
 
-if [ $# -eq 5 ]; then
+if [ $# -eq 4 ]; then
+    signing_key_id="85DD07F5"
     signing_secret_key_ring_file="$1"
-    signing_key_id="$2"
-    signing_key_password="$3"
-    publish_user="$4"
-    publish_password="$5"
+    signing_key_password="$2"
+    publish_user="$3"
+    publish_password="$4"
 
     OPTIONS="publish -Psigning.secretKeyRingFile=${signing_secret_key_ring_file} -Psigning.keyId=${signing_key_id} -Psigning.password=${signing_key_password} -PpublishUser=${publish_user} -PpublishPassword=${publish_password}"
 fi
